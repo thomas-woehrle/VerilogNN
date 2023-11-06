e0cb27ddcf0891ce9ec9f29276d105f9232cc79d
Simon Ruzicka <ruzicsi1@fit.cvut.cz>
2023-11-06 13:42:25 +0100

    Refactoring

--- src/Softmax.v
+++ src/Softmax.v
@@ -3,17 +3,16 @@
 `include "src/FloatingDivision.v"
 
 module Softmax #(parameter SIZE = 4) // IN SIZE should be equal to out size since each value gets a probability
-                (input [(32 * SIZE) - 1 : 0]    in,
+                (input  [(32 * SIZE) - 1 : 0]   in,
                  output [(32 * SIZE) - 1 : 0]   result);
 
-    wire [(32*SIZE) -1: 0] E_storage;
+    wire [(32 * SIZE) - 1: 0] E_storage;
     wire [31:0] sum;
     wire zerodivision;
-    wire [(32*SIZE) -1: 0] probability_storage;
 
     genvar i;
     generate
-        for(i = 0; i <SIZE; i = i +1) begin
+        for(i = 0; i < SIZE; i = i + 1) begin
             e_function E1 (.x_value(in[32 * i +:32]), .result(E_storage[32 * i +: 32]));
         end
     endgenerate
@@ -22,14 +21,13 @@ module Softmax #(parameter SIZE = 4) // IN SIZE should be equal to out size sinc
 
     genvar j;
     generate
-        for(j = 0; j <SIZE; j = j +1) begin
+        for(j = 0; j < SIZE; j = j + 1) begin
             FloatingDivision F1 (.A(E_storage[32 * j +: 32]),
                                  .B(sum),
                                  .zero_division(zerodivision),
-                                 .result(probability_storage[32 * j +: 32]));
+                                 .result(result[32 * j +: 32]));
         end
     endgenerate
 
     // probably some safety measures regarding Zero Div still needed !
-    assign result = probability_storage; 
 endmodule
--- src/VectorSum.v
+++ src/VectorSum.v
@@ -5,25 +5,26 @@
 
 `include "src/FloatingAddition.v"
 
+// outputs the sum of all values in the input vector
 module VectorSum # (parameter VLEN = 4) // ALWAYS ADJUST THE PARAMETER !!!!!!
                    (input  [(32 * VLEN) - 1:0] Vector,
                     output [31:0] result);
 
-    wire [31:0] summe  = 32'b0_00000000_00000000000000000000000;   
     wire [(32 * VLEN) - 1:0] sumvector;
 
     genvar i;
     generate
         for(i = 0; i < VLEN; i = i +1) begin
             if (i == 0) begin
-                FloatingAddition FL(.A(summe), .B(Vector[32* i +: 32]), .result(sumvector[31:0]));
+                // FloatingAddition FL(.A(32'b0_00000000_00000000000000000000000), .B(Vector[32* i +: 32]), .result(sumvector[31:0]));
+                assign sumvector[31:0] = Vector[31:0];
             end
             else begin
-                FloatingAddition FL(.A(sumvector[32*(i-1) +: 32]), .B(Vector[32* i +: 32]), .result(sumvector[32 * i +: 32]));
+                FloatingAddition FL(.A(sumvector[32 * (i-1) +: 32]), .B(Vector[32 * i +: 32]), .result(sumvector[32 * i +: 32]));
             end
         end
     endgenerate
-    assign result = sumvector[32*(VLEN-1) +: 32];
+    assign result = sumvector[32 * (VLEN-1) +: 32];
 endmodule;
 
 `endif // _vector_addition
