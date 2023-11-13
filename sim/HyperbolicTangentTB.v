67206f2b8a0bb47099b168d856029e172c2b7021
Philippwon <72304275+Philippwon@users.noreply.github.com>
2023-11-13 23:20:36 +0100

    Testbench for the Hyperbolic tangent Function

---
+++ sim/HyperbolicTangentTB.v
@@ -0,0 +1,29 @@
+`timescale 1ns/1ns
+`include "HyperbolicTangent.v"
+
+
+module HyperbolicTangentTB;
+    reg [31:0] Vector;
+    wire [31:0] result;
+
+    HyperbolicTangent H1(Vector,result);
+
+    initial 
+    begin
+        $dumpfile("HyperbolicTangentTB.vcd");
+        $dumpvars(0,HyperbolicTangentTB);
+
+        #20;
+        Vector = 32'b11000000000000000000000000000000;//-2
+        #20;
+        Vector = 32'b01000000000000000000000000000000;// 2
+        #20;
+        Vector = 32'b01000000100000000000000000000000;// 4
+        #20;
+        Vector = 32'b01000000100000000000000000000000;// 13
+        #20;
+        Vector = 32'b01000001110010000000000000000000;// 25
+        #20;
+        $display("%b",result);
+    end
+endmodule
