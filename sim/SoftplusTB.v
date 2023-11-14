85f8e80e807f1ed01a8fa9a772554c12a09a4473
Philippwon <72304275+Philippwon@users.noreply.github.com>
2023-11-14 15:47:24 +0100

    Testbench for the Softplus function

---
+++ sim/SoftplusTB.v
@@ -0,0 +1,36 @@
+`timescale 1ns/1ns
+`include "Softplus.v"
+
+
+module SoftplusTB;
+    reg [31:0] Vector;
+    wire [31:0] result;
+
+    Softplus S1(Vector,result);
+
+    initial 
+    begin
+        $dumpfile("SoftplusTB.vcd");
+        $dumpvars(0,SoftplusTB);
+
+        #20;
+        Vector = 32'b11000000000000000000000000000000;//-2
+        #20;
+        Vector = 32'b10111111100000000000000000000000;//-1
+        #20;
+        Vector = 32'b00000000000000000000000000000000;// 0
+        #20;
+        Vector = 32'b00111111100000000000000000000000;// 1
+        #20;
+        Vector = 32'b01000000000000000000000000000000;// 2
+        #20;
+        Vector = 32'b01000000100000000000000000000000;// 4
+        #20;
+        Vector = 32'b01000001010100000000000000000000;// 13
+        #20;
+        Vector = 32'b01000001110010000000000000000000;// 25
+        #20;
+
+        $display("%b",result);
+    end
+endmodule
