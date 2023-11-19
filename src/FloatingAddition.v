82e3ab13e3712b3f98e9fc6cb15f269999ac19b8
Simon Ruzicka <ruzicsi1@fit.cvut.cz>
2023-11-19 18:07:53 +0100

    Fixes in FloatAddition, VectorAddition
    Based on complaints from Vivado synthesizer (still not working, but for different reasons).

--- src/FloatingAddition.v
+++ src/FloatingAddition.v
@@ -19,6 +19,8 @@ module FloatingAddition (input [31:0]A,
     reg [2:0] one_hot;
     wire comp;
 
+    integer i;
+
     // compare absolute values of A, B
     FloatingCompare comp_abs(.A({1'b0, A[30:0]}), .B({1'b0, B[30:0]}), .result(comp));
 
@@ -45,10 +47,15 @@ module FloatingAddition (input [31:0]A,
             result_Mantissa = result_Mantissa >> 1;
             result_Exponent = result_Exponent + 1;
         end
+        else if(|result_Mantissa != 1'b1) begin  // mantissa contains no 1 or unknown value (result should be 0, while loop below would get stuck)
+            result_Exponent = 0;  // 2 ** (0-127) is almost 0
+        end
         else begin
-            while(!result_Mantissa[23]) begin
+            i = 0;  // fixed limit of iterations because Vivado saw this as an infinite loop
+            while(result_Mantissa[23] !== 1'b1 && result_Exponent > 0 && i < 24) begin  // 1st bit is not 1, but there is some 1 in the mantissa
                 result_Mantissa = result_Mantissa << 1;
                 result_Exponent = result_Exponent - 1;
+                i = i + 1;
             end
         end
 
--- src/VectorAddition.v
+++ src/VectorAddition.v
@@ -11,14 +11,11 @@ module VectorAddition #(parameter VLEN = 1)
                         input  [(32 * VLEN) - 1:0] B,
                         output [(32 * VLEN) - 1:0] result);
 
-    genvar i;
-    generate
-        for(i = 0; i < VLEN; i = i + 1) begin
+        for(genvar i = 0; i < VLEN; i = i + 1) begin
             wire [31:0] A_item = A[32 * i +: 32];
             wire [31:0] B_item = B[32 * i +: 32];
 
             FloatingAddition add1(.A(A_item), .B(B_item), .result(result[32 * i +: 32]));  // element-wise addition
         end
-    endgenerate
 endmodule
 `endif // _vector_addition
