5b729b44bc5b319013185ee9d3ff8f570a3db321
Simon Ruzicka <ruzicsi1@fit.cvut.cz>
2023-12-17 10:23:59 +0100

    Clock Divider

---
+++ sim_vivado/ClockDivider.v
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+
+// Divides the input clock frequency by 2 ** RATIO_EXP. Stores a register
+// of RATIO_EXP bits, incremented by one on input clock and outputs the
+// highest bit.
+module ClockDivider #(RATIO_EXP = 1)
+                        (input clk_in,
+                        output clk_out);
+    reg [RATIO_EXP - 1:0] ctr;
+    assign clk_out = ctr[RATIO_EXP - 1];
+
+    always @ (posedge clk_in) begin
+        ctr = ctr + 1;
+    end
+
+endmodule
