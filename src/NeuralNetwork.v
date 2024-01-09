fe1dc8d6cad9d2aa75e6887b4fb1d5de759a415c
Simon Ruzicka <ruzicsi1@fit.cvut.cz>
2024-01-09 14:57:53 +0100

    Naming convention, trailing WS

--- src/NeuralNetwork.v
+++ src/NeuralNetwork.v
@@ -18,18 +18,18 @@ for (genvar PK_IDX=0; PK_IDX<(PK_LEN); PK_IDX=PK_IDX+1) begin \
     assign PK_DEST[(PK_WIDTH)*PK_IDX +: PK_WIDTH] = PK_SRC[PK_IDX][((PK_WIDTH)-1):0]; \
 end
 
-// The parameters MAXWEIGHTS and MAXNEURONS are used to find the maximum size of a single layer of the NN. 
+// The parameters MAXWEIGHTS and MAXNEURONS are used to find the maximum size of a single layer of the NN.
 // Due to the structure of the NN, this should then be sufficient to carry out all the necessary calculations.
-// The net_arch input variable contains the number of neurons located on each level of the NN. 
-// The main advantage of this module is the drastically increased speed at which you can build a new network 
+// The net_arch input variable contains the number of neurons located on each level of the NN.
+// The main advantage of this module is the drastically increased speed at which you can build a new network
 // with a specific architecture. This now only requires the specification of the parameters mentioned above.
-module NeuralNetworkNew #(parameter NR_LAYERS = 2, INPUTSIZE = 4, OUTPUTSIZE = 10, MAXWEIGHTS= 4, MAXNEURONS= 10)
+module NeuralNetwork #(parameter NR_LAYERS = 2, INPUTSIZE = 4, OUTPUTSIZE = 10, MAXWEIGHTS= 4, MAXNEURONS= 10)
                         (input [(32 * INPUTSIZE) - 1: 0]  inputdata,
                         input [(32 * NR_LAYERS) - 1: 0]  net_arch,
                         input                            clk,
                         output reg[(32 * OUTPUTSIZE) - 1: 0] result);
 
-wire [32*MAXNEURONS -1:0] currentresult;      // contains result of the current layer  
+wire [32*MAXNEURONS -1:0] currentresult;      // contains result of the current layer
 wire [32*MAXNEURONS-1:0] mul_store;   // contains the result after the addition of the biases
 wire [32*MAXNEURONS-1:0] add_store;   // contains the result after the multiplication process
 reg [32*MAXNEURONS-1:0] data_store;   // contains the data before each layer computation cycle
@@ -61,7 +61,7 @@ MatrixMultiplicationFlex #( .LBUF(MAXNEURONS),
                                 .result(mul_store),
                                 .done(donemul));
 
-VectorAdditionFlex #( .LBUF(MAXNEURONS)) 
+VectorAdditionFlex #( .LBUF(MAXNEURONS))
                             VA1 (
                                 .A(mul_store),
                                 .B(biasstorage),
@@ -76,12 +76,12 @@ genvar i;
 generate
     for(i = 0; i < MAXNEURONS; i = i + 1)
         Sigmoid sigmoid(.num(add_store[32 * i +: 32]), .result(currentresult[32 * i +: 32]));
-endgenerate 
+endgenerate
 
 always @(doneadd) begin
-    if (layerindex >= NR_LAYERS) begin // determines when to stop the computation 
+    if (layerindex >= NR_LAYERS) begin // determines when to stop the computation
         result = currentresult; // final result is then loaded onto the output signal.
-    end 
+    end
     else begin
         // The process of reading data is always carried out at the beginning on every level of the NN
         // The data needs to be in the correct format in the pre defined location.
@@ -95,7 +95,7 @@ always @(doneadd) begin
             neuron_cnt <= net_arch[0+:32];
         end
         else begin
-            data_store <= currentresult; 
+            data_store <= currentresult;
             input_cnt <= neuron_cnt;
             neuron_cnt <= net_arch [(layerindex-1)*32+:32];
         end
