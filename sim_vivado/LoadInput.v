c8b34279c77e4944d6b7728baf648a268c50ae4e
Simon Ruzicka <ruzicsi1@fit.cvut.cz>
2024-02-16 16:19:51 +0100

    LoadInput
    Purely for debugging (simulation) reasons

---
+++ sim_vivado/LoadInput.v
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ps
+
+// Outputs a vector of floating point numbers in designated format.
+module LoadInput (output [(32 * 784) - 1:0] img);
+    reg [31:0] arr [0:783];
+
+    for(genvar i = 0; i < 784; i = i + 1) begin
+        assign img[32 * i +: 32] = arr[i][31:0];
+    end
+
+    initial begin
+        $readmemb("/usr/prakt/w0029/NN.FPGA/Codebase/data/img/input00.mem", arr, 0, 783);
+    end
+
+endmodule
