d46a380392b3dbcce027a76ee44d60d8de4a4d9f
Tom Woehrle <thomas.e.woehrle@gmail.com>
2024-11-22 15:22:30 +0100

    Add unrefactored test for FloatingMultiplication

--- sim/test_FloatingMultiplication.py
+++ sim/test_FloatingMultiplication.py
@@ -9,6 +9,9 @@ import struct
 import random
 
 
+IEEE754_MAX_VAL = 3.4028235e38
+
+
 def float_to_ieee754(value: float) -> int:
     """Convert a float to its IEEE 754 single-precision binary representation as an Integer"""
     return struct.unpack('>I', struct.pack('>f', value))[0]
@@ -45,25 +48,27 @@ async def run_test(dut, a, b):
 
     await Timer(1)
 
-    result = dut.result.value
-    expected = 1 if a >= b else 0
+    result = ieee754_to_float(dut.result.value)
+    expected = a * b
+    tolerance = get_tolerance(a * b)
 
-    assert result == expected, \
-        f"Mismatch: {a} >= {b} = {result} (expected {expected})"
+    assert abs(result - expected) < tolerance, \
+        f"Mismatch: {a} * {b} = {result} (expected {expected})"
 
 
 @cocotb.test()
-async def test_compare_basic(dut):
+async def test_multiplication_basic(dut):
     await run_test(dut, 0.0, 0.0)
     await run_test(dut, 1.0, 1.0)
     await run_test(dut, -1.0, -1.0)
     await run_test(dut, 1.0, -1.0)
     await run_test(dut, -1.0, 1.0)
+    await run_test(dut, 0.5, 0.5)
 
 
 @cocotb.test()
-async def test_random_compare_simple(dut):
-    """Test floating point comparison with simple random values"""
+async def test_random_multiplication_simple(dut):
+    """Test floating point multiplication with simple random values"""
     for _ in range(100):
         # Generate random float values
         a = random.uniform(-100, 100)
@@ -73,13 +78,13 @@ async def test_random_compare_simple(dut):
 
 
 @cocotb.test()
-async def test_random_compare_full_range(dut):
-    """Test floating point comparison with random values from the full range"""
+async def test_random_add_full_range(dut):
+    """Test floating point multiplication with random values from the full range"""
+    max_safe_val = math.sqrt(IEEE754_MAX_VAL)
     for _ in range(1000):
         # Generate random float values
-        # 3.4028235e38 is the highest value in ieee754 single precision float.
-        a = random.uniform(-3.4e38, 1.7e38)
-        b = random.uniform(-3.4e38, 1.7e38)
+        a = random.uniform(-max_safe_val, max_safe_val)
+        b = random.uniform(-max_safe_val, max_safe_val)
 
         await run_test(dut, a, b)
 
@@ -89,16 +94,16 @@ def test_runner():
 
     proj_path = Path(__file__).resolve().parent.parent / "src"
 
-    sources = [proj_path/"FloatingCompare.v"]
+    sources = [proj_path/"FloatingMultiplication.v"]
 
     runner = get_runner(sim)
     runner.build(
         sources=sources,
-        hdl_toplevel="FloatingCompare",
+        hdl_toplevel="FloatingMultiplication",
     )
 
-    runner.test(hdl_toplevel="FloatingCompare",
-                test_module="test_FloatingCompare")
+    runner.test(hdl_toplevel="FloatingMultiplication",
+                test_module="test_FloatingMultiplication")
 
 
 if __name__ == "__main__":
